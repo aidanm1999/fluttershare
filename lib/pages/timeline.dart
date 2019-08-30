a4011151ca58ec90e516a8d443175bb635c4d80d
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-08-30 12:21:35 +0100

    17 - Simple and Compound Queries in Firestore

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +11,25 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef.limit(1).getDocuments();
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+    });
+  }
+
+  getUserById() async {
+    final String id = "oVMMNxtbuvB6NcrmlFdD";
+    final DocumentSnapshot doc = await usersRef.document(id).get();
+    print(doc.data);
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
