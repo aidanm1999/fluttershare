27f1e40f529946fa1af02315cdd81b5912b6bc55
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-08-31 17:33:18 +0100

    fixed context bug

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,65 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
 }
 
-AppBar buildSearchField() {
-  return AppBar(
-    backgroundColor: Colors.white,
-    title: TextFormField(
-      decoration: InputDecoration(
-        hintText: "Search for a user",
-        filled: true,
-        prefixIcon: Icon(
-          Icons.account_box,
-          size: 28,
-        ),
-        suffixIcon: IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: () => print("Cleared"),
-        ),
-      ),
-    ),
-  );
-}
-
-Container buildNoContent(BuildContext context) {
-  final Orientation orientation = MediaQuery.of(context).orientation;
-  return Container(
-    child: Center(
-      child: ListView(
-        shrinkWrap: true,
-        children: <Widget>[
-          SvgPicture.asset(
-            'assets/images/search.svg',
-            height: orientation == Orientation.portrait ? 300 : 200,
-          ),
-          Text(
-            "Find Users",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w600,
-              fontSize: 60,
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
 class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: "Search for a user",
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print("Cleared"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/search.svg',
+              height: orientation == Orientation.portrait ? 300 : 200,
+            ),
+            Text(
+              "Find Users",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 60,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
-      body: buildNoContent(context),
+      body: buildNoContent(),
     );
   }
 }
