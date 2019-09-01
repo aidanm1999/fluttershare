bdf1f8d15113e864663a852d1fa626f9aba0cf95
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-09-01 18:28:51 +0100

    29 - Building Upload Form

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -123,7 +123,7 @@ class _HomeState extends State<Home> {
             onPressed: logout,
           ),
           ActivityFeed(),
-          Upload(),
+          Upload(currentUser: currentUser),
           Search(),
           Profile(),
         ],
