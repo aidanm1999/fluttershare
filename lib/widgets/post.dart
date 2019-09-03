62acc5446d09f42cec02a8d6457f91cdcd5e50e9
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2019-09-03 12:14:55 +0100

    43 - Building Comments Page Functionality

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -5,6 +5,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/comments.dart';
 import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/pages/timeline.dart';
 import 'package:fluttershare/widgets/custom_image.dart';
@@ -217,7 +218,12 @@ class _PostState extends State<Post> {
               ),
             ),
             GestureDetector(
-              onTap: () => print('show comments'),
+              onTap: () => showComments(
+                context,
+                postId: postId,
+                ownerId: ownerId,
+                mediaUrl: mediaUrl,
+              ),
               child: Icon(
                 Icons.chat,
                 size: 28,
@@ -274,4 +280,20 @@ class _PostState extends State<Post> {
       ],
     );
   }
+}
+
+showComments(BuildContext context,
+    {String postId, String ownerId, String mediaUrl}) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) {
+        return Comments(
+          postId: postId,
+          postOwnerId: ownerId,
+          postMediaUrl: mediaUrl,
+        );
+      },
+    ),
+  );
 }
